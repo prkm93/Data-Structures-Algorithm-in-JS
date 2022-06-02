b9eeef176c3bec76dd6890a3e96da0530e6b2796
prkm93 <aradshub93@gmail.com>
2022-06-02 09:35:29 +0530

    polyfill for filter

--- JS/Polyfills/polyfills.js
+++ JS/Polyfills/polyfills.js
@@ -19,3 +19,17 @@ Array.prototype.ownMap = function(callback) {
   
   return newArr;
 }
+
+/** filter polyfill */
+
+Array.prototype.ownFilter = function(callback, context) {
+  let newArr = [];
+  
+  for (let i = 0; i < this.length; i++) {
+      if (callback.call(context, this[i], i, this)) {
+        newArr.push(this[i]);
+      }
+  }
+
+  return newArr;
+}
